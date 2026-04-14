b6f0fe36519a8c6b79cc31835d6fb19c4304bea3
Truong Nguyen Van <truongdev203@gmail.com>
2026-04-14 21:06:10 +0700

    day code

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
